b06572bc31d417323f5904e2525c5562a1089bef
tariqalimahar <tariqalimahar2007@gmail.com>
2024-05-20 21:44:51 +0500

    javaclass2

---
+++ index.js
@@ -0,0 +1,88 @@
+    //  let num =prompt("what is");
+    //  console.log(+num +5);
+// comperison operator
+// // == ===
+// let num1 = 6; //data type number
+// let num2 = '6'; //string
+// console.log(num1 == num
+// let num1 = 6; //data type number
+//  let num2 = '6'; //string
+//  console.log(num1 === num2);
+
+// let num1 = 6; //data type number
+//  let num2 = '6'; //string
+//  console.log(num1 !== num2);
+// // console.log("5" !== 5)
+// < greater then
+// > less then
+
+// console.log(5 > 7);
+// console.log(5 < 7);
+
+// let num = 55;
+// let num2 = 66;
+// console.log(num < num2);
+
+
+// let num = 155;
+// let num2 = 66;
+// console.log(num > num2);
+
+
+// <= <== 
+
+
+// let num = 155;
+// let num2 = 66;
+// console.log(num <= num2);
+
+
+
+
+// let num = 155;
+// let num2 = 66;
+// console.log(num >= num2);
+
+
+// let num = 155;
+// let num2 = 154;
+// console.log(num <= num2);
+
+
+
+
+
+
+// let num = 155;
+// let num2 = 9999;
+// console.log(num <= num2);
+
+
+// let age = +prompt("inter your age");
+
+// if(age >= 18 ){
+//     console.log(" you can apply cnic");
+// }else{
+//     console.log("you are not eligble")
+// }
+
+
+// let age = +prompt("inter your age");
+
+// if(age >= 18 ){
+//     console.log(" you can apply cnic");
+// }else{
+//     console.log("you are not eligble")
+// }
+
+// let userNum = +prompt("inter your number");
+// let computerNumber = Math.round(Math.random() * 5)
+// if(userNum === computerNumber){
+//     console.log("you win");
+// }else {
+//     console.log("you loss !"+ computerNumber);  
+// }
+
+
+
+
